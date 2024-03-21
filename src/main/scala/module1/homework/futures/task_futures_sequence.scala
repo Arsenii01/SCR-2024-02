046f01bbd0449b7631a3661245a819169ab924c1
Arseniy <arsa1200@yandex.ru>
2024-03-21 21:23:00 +0300

    HW 5 done

--- src/main/scala/module1/homework/futures/task_futures_sequence.scala
+++ src/main/scala/module1/homework/futures/task_futures_sequence.scala
@@ -2,6 +2,7 @@ package module1.homework.futures
 
 import module1.homework.futures.HomeworksUtils.TaskSyntax
 
+import scala.annotation.tailrec
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
 
@@ -16,10 +17,34 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = ???
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    @tailrec
+    def loop(remaining: List[Future[A]],
+             acc: Future[(List[A], List[Throwable])]): Future[(List[A], List[Throwable])] = {
+      remaining match {
+        case Nil => acc
+        case head :: tail =>
+          val p = Promise[(List[A], List[Throwable])]
+          acc.onComplete {
+            case Success((successValues, failureValues)) =>
+              head.onComplete {
+                case Failure(exception) =>
+                  p.success((successValues, failureValues :+ exception))
+                case Success(value) =>
+                  p.success((successValues :+ value, failureValues))
+              }
+            case Failure(ex) =>
+              p.failure(ex)
+          }
+          loop(tail, p.future)
+      }
+    }
+    loop(futures, Future.successful(List.empty[A], List.empty[Throwable]))
+  }
 }
